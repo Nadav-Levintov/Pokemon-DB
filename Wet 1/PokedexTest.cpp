a153e4d9f6673fe5a0f6098959557fc16947df79
Nadav-Levintov <DSP155@Gmail.com>
2015-12-01 20:35:02 +0200

    Added test for LevelUP

--- Wet 1/PokedexTest.cpp
+++ Wet 1/PokedexTest.cpp
@@ -4,20 +4,37 @@
 
 using namespace std;
 
-bool basicPokadexTest()
+bool basicCatchFreeTest()
 {
 	Pokedex DS=Pokedex();
 	DS.addTrainer(1);
 	DS.catchPokemon(1, 1, 1);
 	int poke= DS.getMaxPokemonId();
 	ASSERT_EQUALS(1, poke);
+	DS.freePokemon(1);
+	poke = DS.getMaxPokemonId();
+	ASSERT_EQUALS(-1, poke);
+	return true;
+}
+
+bool basicLevelUpTest()
+{
+	Pokedex DS = Pokedex();
+	DS.addTrainer(1);
+	DS.catchPokemon(1, 1, 1);
+	DS.catchPokemon(2, 1, 3);
+	int poke = DS.getMaxPokemonId();
+	ASSERT_EQUALS(2, poke);
+	DS.levelUp(1, 5);
+	poke = DS.getMaxPokemonId();
+	ASSERT_EQUALS(1, poke);
 	return true;
 }
 
 int main()
 {
-	RUN_TEST(basicPokadexTest);
-
+	RUN_TEST(basicCatchFreeTest);
+	RUN_TEST(basicLevelUpTest);
 
 	return 0;
 }
